01dbb6fd66d91984620e5ad29f42cdeae7549716
kskotenko <kskotenko@gmail.com>
2022-09-22 19:33:20 +0300

    change page title; add author link to github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
 		<div className="App">
 			<h1>Weather App</h1>
 			<SearchForm />
+			<div className="AuthorDiv">
+				<a
+					className="AuthorLink"
+					href="https://github.com/kse-kot/weather-app-react"
+					target="_blank"
+				>
+					Open-source
+				</a>{' '}
+				code by <strong>Kseniia K</strong>
+			</div>
 		</div>
 	)
 }
